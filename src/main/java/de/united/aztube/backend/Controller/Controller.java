18c6df8d60b6a54219d23588a4132e6c59e1966a
Kevin Fischer <kfischer@united-internet.org>
2022-01-10 16:04:02 +0100

    test controller for ytdl

--- src/main/java/de/united/aztube/backend/Controller/Controller.java
+++ src/main/java/de/united/aztube/backend/Controller/Controller.java
@@ -1,5 +1,6 @@
-package de.united.aztube.backend;
+package de.united.aztube.backend.Controller;
 
+import de.united.aztube.backend.CodeGenerator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/de/united/aztube/backend/Controller/YTDLController.java
@@ -0,0 +1,17 @@
+package de.united.aztube.backend.Controller;
+
+import de.united.aztube.backend.CodeGenerator;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/ytdl")
+public class YTDLController {
+
+    @GetMapping(path = "/dosomething")
+    public void doSomething() {
+        System.out.println("Hello from Srping backend");
+    }
+
+}
