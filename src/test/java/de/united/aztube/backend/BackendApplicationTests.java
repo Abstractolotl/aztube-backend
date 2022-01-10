299fa5410d02306d022faf5147f77101e2542e4c
Kevin Fischer <kfischer@united-internet.org>
2022-01-10 10:24:30 +0100

    spring init

---
+++ src/test/java/de/united/aztube/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.united.aztube.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
