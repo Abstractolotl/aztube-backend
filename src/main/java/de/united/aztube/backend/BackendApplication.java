0bb9d84adaec7cf28e80b2081a35d1fe160c1ca0
NouCake <KevinFischer2010@gmail.com>
2022-01-24 16:29:15 +0100

    refactor

--- src/main/java/de/united/aztube/backend/BackendApplication.java
+++ src/main/java/de/united/aztube/backend/BackendApplication.java
@@ -1,11 +1,17 @@
 package de.united.aztube.backend;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -15,7 +21,7 @@ public class BackendApplication {
 	}
 
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
+	public WebMvcConfigurer configuration() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
