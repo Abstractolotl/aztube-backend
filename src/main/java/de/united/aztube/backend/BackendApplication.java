2471fd985ce863c229b3bf09f26bf1e4790b07f9
Kevin Fischer <kfischer@united-internet.de>
2023-02-01 09:51:40 +0100

    Hide Secrets

--- src/main/java/de/united/aztube/backend/BackendApplication.java
+++ src/main/java/de/united/aztube/backend/BackendApplication.java
@@ -1,7 +1,11 @@
 package de.united.aztube.backend;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,8 +16,6 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
-    
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
